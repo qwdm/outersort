442ef8a070fc1a76802e4425316c2479d6f82121
Dime Mikhalchenko <mixal4enko@gmail.com>
2018-02-13 22:42:53 +0300

    auto split

--- outersort.py
+++ outersort.py
@@ -4,12 +4,36 @@ import random
 import bisect
 import os
 import sys
+import re
 
 
-def get_quantiles(filename, n_splits, strnum):
-    desirable_sample_size = min(n_splits * 1000, 100000)
+def get_n_splits(filename):
+
+    filesize = os.path.getsize(filename)  # in bytes
+
+    try:
+        meminfo = open('/proc/meminfo').read()
+        matched = re.search(r'MemAvailable:\s+(\d+)', meminfo)
+        mem_available = int(matched.groups()[0]) * 1024  # kB -> bytes
+    except:
+        mem_available = 1024 * 1024 * 1024  # 1GB has almost any machine nowdays (c)
+        print('available memory set to 1GB, cannot measure memory', file=sys.stderr)
+
+    n_splits = 10 * (filesize // mem_available + 1)  # 10 just for test purpouses, it takes long time to test on really big data
+                                                     # and for less memory usage while working
+
+    ## if filesize < mem_available one can sort file just in memory, but we want to test our algorightm
+
+    return n_splits
+
+
+def get_quantiles(filename, n_splits):
+
+    filesize = os.path.getsize(filename)  
+    
+    alpha = min(1, (10 * 1024 * 1024 / filesize))
+
     sample = []
-    alpha = desirable_sample_size / strnum
     with open(filename) as infile:
         for line in infile:
             if random.random() < alpha:
@@ -28,12 +52,9 @@ def make_split_filenames(tmpdir, n_splits):
     return [os.path.join(tmpdir, 'split%04d' % n) for n in range(n_splits)]
 
 
-def make_splits(filename, tmpdir, n_splits, strnum):
+def make_splits(filename, tmpdir, n_splits):
 
-    quantiles = get_quantiles(filename, n_splits, strnum)
-
-    split_counters = [0] * n_splits
-
+    quantiles = get_quantiles(filename, n_splits)
     
     try:
         infile = open(filename)
@@ -42,7 +63,6 @@ def make_splits(filename, tmpdir, n_splits, strnum):
         for line in infile:
             index = bisect.bisect_left(quantiles, line)
             splits[index].write(line)
-            split_counters[index] += 1
 
     finally:
         infile.close()
@@ -71,12 +91,17 @@ def merge(filename, tmpdir, n_splits):
 
             os.remove(split_filename)
 
+    os.remove(filename)
+    os.rename(filename + '_tmp', filename)
+
 
 def main():
 
+    # simple sys.argv, argparse is overkill here
     filename = sys.argv[1]
-    n_splits = int(sys.argv[2])
-    strnum = int(sys.argv[3])
+    n_splits = int(sys.argv[2]) if len(sys.argv) == 3 else get_n_splits(filename)
+
+    print('Use %s splits' % n_splits, file=sys.stderr)
 
     tmpdir = os.path.dirname(os.path.abspath(filename))
     tmpdir = os.path.join(tmpdir, filename + '_tmpdir')
@@ -87,7 +112,7 @@ def main():
         else:
             sys.exit('%s directory exists, cannot use this path as tmpdir' % tmpdir)
 
-        make_splits(filename, tmpdir, n_splits, strnum)
+        make_splits(filename, tmpdir, n_splits)
         sort_splits(tmpdir, n_splits)
         merge(filename, tmpdir, n_splits)
 
